f463ad611eb6e76982a8062bb1c02c69c7f1dc7a
Taiyo86-create <154989695+Taiyo86-create@users.noreply.github.com>
2024-04-30 23:55:05 +0900

    初期化

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDtUqPx_MdpaBmMFqMVisut4IuMaj6nZCM",
+  authDomain: "fir-tutorial-8e2c1.firebaseapp.com",
+  projectId: "fir-tutorial-8e2c1",
+  storageBucket: "fir-tutorial-8e2c1.appspot.com",
+  messagingSenderId: "852424363381",
+  appId: "1:852424363381:web:ef94062e6fb4d44826973c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
