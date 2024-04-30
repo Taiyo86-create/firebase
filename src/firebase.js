53c4d526b8bea44520227f9b30cff04d140846a3
Taiyo86-create <154989695+Taiyo86-create@users.noreply.github.com>
2024-05-01 00:09:00 +0900

    firebaseと連携

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,5 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
+import { getFirestore } from "firebase/firestore"
 const firebaseConfig = {
   apiKey: "AIzaSyDtUqPx_MdpaBmMFqMVisut4IuMaj6nZCM",
   authDomain: "fir-tutorial-8e2c1.firebaseapp.com",
@@ -13,5 +9,6 @@ const firebaseConfig = {
   appId: "1:852424363381:web:ef94062e6fb4d44826973c"
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
