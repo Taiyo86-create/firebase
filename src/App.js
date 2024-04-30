11d73a22fd29cfd7ca313f5f462d739292388bfc
Taiyo86-create <154989695+Taiyo86-create@users.noreply.github.com>
2024-05-01 00:21:45 +0900

    データの取得に成功

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import db from "./firebase";
+import { collection, getDocs } from "firebase/firestore";
 
 function App() {
+  const [posts, setPosts] = useState([]);
+  
+  useEffect(() => {
+    const postData = collection(db, "posts");
+    getDocs(postData).then((snapShot)=>{
+      console.log(snapShot.docs.map((doc) => doc.data()));
+    });
+  }, []);
+
   return (
     <div className="App">
-      こんにちは
+      
     </div>
   );
 }
